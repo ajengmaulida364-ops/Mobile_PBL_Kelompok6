5f5075d46118378dd3824fc2b0ee778b50df3459
kifiyahfaiqotul76 <kifiyahfaiqotul76@gmail.com>
2026-05-24 16:33:48 +0700

    auth 2

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,46 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class ApiService {
 
   Future login({
+
     required String login,
     required String password,
+
   }) async {
 
-    // NANTI CONNECT LARAVEL
+    final response = await http.post(
 
-    print(login);
-    print(password);
+      Uri.parse(
+        'http://127.0.0.1:8000/api/login',
+      ),
 
+      headers: {
+        'Accept': 'application/json',
+      },
+
+      body: {
+
+        'login': login,
+        'password': password,
+
+      },
+    );
+
+    print(response.statusCode);
+
+    print(response.body);
+
+    if (response.body.isEmpty) {
+
+      return {
+        'status': false,
+        'message': 'Response kosong'
+      };
+    }
+
+    return jsonDecode(response.body);
   }
 }
